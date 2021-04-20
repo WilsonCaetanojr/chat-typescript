41600390e5fbb7ecbe12bc5d2c97bfcc5776f1d3
WilsonCaetano-Qyon <wilsonjr_cartano@hotmail.com>
2021-04-20 17:10:08 -0300

    Configura entidades

---
+++ src/entities/Setting.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("setting")
+class Setting {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  chat: boolean;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Setting };
